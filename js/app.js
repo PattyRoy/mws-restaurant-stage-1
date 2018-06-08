a8b300c7a83e53099285c01b92cdad4310b53c83
PattyRoy <PatriciaRoy@acm.org>
2018-06-08 16:38:17 -0400

    Changes made based on review. - map is on top when in mobile view - filter options are not covered up in mobile view - alt tag added to images on restaurant page - app.js added to html pages

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/sw.js').then(function(reg) {
+  navigator.serviceWorker.register('sw.js').then(function(reg) {
     console.log('Yay, service worker registered');
   }).catch(function(err) {
     console.log('Boo, service worker registration failed', err);
--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -58,7 +58,8 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-
+  image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = "image of " + restaurant.name;
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
 

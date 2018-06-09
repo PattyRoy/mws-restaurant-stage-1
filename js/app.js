915dc854b92b4bd9dd963abb4184ee6524890e8b
PattyRoy <PatriciaRoy@acm.org>
2018-06-09 11:23:47 -0400

    Service worker is working.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('sw.js').then(function(reg) {
+  navigator.serviceWorker.register('/sw.js').then(function(reg) {
     console.log('Yay, service worker registered');
   }).catch(function(err) {
     console.log('Boo, service worker registration failed', err);
   });
+}
--- sw.js
+++ sw.js
@@ -29,6 +29,16 @@ var urlsToCache = [
   '/js/main.js',
   '/js/restaurant_info.js',
   '/restaurant.html',
+  '/restaurant.html?id=1',    
+  '/restaurant.html?id=2',    
+  '/restaurant.html?id=3',    
+  '/restaurant.html?id=4',    
+  '/restaurant.html?id=5',    
+  '/restaurant.html?id=6',    
+  '/restaurant.html?id=7',    
+  '/restaurant.html?id=8',    
+  '/restaurant.html?id=9',    
+  '/restaurant.html?id=10',    
 ];
 
 self.addEventListener('install', function (event) {
@@ -41,19 +51,34 @@ self.addEventListener('install', function (event) {
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
+       // Cache hit - return response
        if (response) {
           return response;
        }
        else {
-          return fetch(event.request).then(function (response) {
-             let responseClone = response.clone();
-        
+          // IMPORTANT: Clone the request. A request is a stream and
+          // can only be consumed once. Since we are consuming this
+          // once by cache and once by the browser for fetch, we need
+          // to clone the response.
+          let responseClone = event.request.clone();
+          return fetch(responseClone).then(function (response) { 
+             // Check if we received a valid response
+             if(!response || response.status !== 200 || response.type !== 'basic') {
+               return response;
+             }
+             // IMPORTANT: Clone the response. A response is a stream
+             // and because we want the browser to consume the response
+             // as well as the cache consuming the response, we need
+             // to clone it so we have two streams.
+             var responseToCache = response.clone();
              caches.open(restaurantCacheName).then(function (cache) {
-                cache.put(event.request, responseClone);
+                cache.put(event.request, responseToCache);
              });
              return response;
-          })
-       }
+          });
+       }       
+    }));
 });
+
 
 
